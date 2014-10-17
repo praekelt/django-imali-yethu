e4d9d091bed398c524c35eb0f39d5631db21ece3
Rudi Giesler <rudi@praekelt.com>
2014-10-17 20:53:23 +0200

    Added snappy bouncer to list of urls on front page

--- imaliyethu/views.py
+++ imaliyethu/views.py
@@ -13,5 +13,8 @@ class ApiRoot(APIView):
             'api-auth': reverse('rest_framework:login', request=request),
             'toilet_codes': reverse('toilet_codes_list', request=request),
             'toilet_issues': reverse('toilet_issues_list', request=request),
-            # 'snappy_bouncer': reverse('snappybouncer', request=request),
+            'snappy_bouncer': '%s?format=json' % request.build_absolute_uri(
+                reverse(
+                    'api_v1/snappybouncer_top_level',
+                    kwargs={'api_name': 'v1/snappybouncer'}, request=request)),
             })
